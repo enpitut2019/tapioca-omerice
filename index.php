959dac2ce4e1edbf422253a9f6549368cc45abcc
tano7 <tano.16.7.4@gmail.com>
2019-08-01 15:34:17 +0900

    db接続

--- index.php
+++ index.php
@@ -16,6 +16,18 @@
 
 <a href = "http://tapiome.herokuapp.com/store_info.php">店舗情報</a>
 
+<?php
+  $url = parse_url(getenv('DATABASE_URL'));
+
+  $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+  $stmt = $pdo->query("SELECT * FROM sample0801_db");
+  $row = $stmt -> fetch(PDO::FETCH_ASSOC
+
+  echo $row["store_id"];
+  
+ ?>
 
 </body>
 </html>
