e4be414601eb81e3b04327b7e35623c0ea2ce89d
RikiyaShinkai <rickey.bb0918@gmail.com>
2019-08-01 13:53:06 +0900

    検索結果

---
+++ result.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Good Bye tapioka</title>
+</head>
+<body>
+  <h1>検索結果</h1>
+
+<a href = "http://tapiome.herokuapp.com/store_info.php">店舗情報</a>
+</body>
+</html>
