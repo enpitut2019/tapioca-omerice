6543e1cbccdd64d92a70a845c983f2363569e064
Shiknai <rickey.bb0918@gmail.com>
2019-08-05 15:15:11 +0900

    グレー

--- store_info_count.php
+++ store_info_count.php
@@ -60,38 +60,34 @@ $date = date("H");
 // 投票数の表示
 for ($i = 0; $i < 12; $i++) {
   if($i ==(intval($date)/2)) {
-    echo '<strong>';
+
     if($result_vote[$o_key[$i]] > $result_vote[$c_key[$i]]){
+      echo '<strong>';
       echo $time[$i].' ... ';
       echo '<span class="red">'.$result_vote[$o_key[$i]].'</span> : ';
       echo $result_vote[$c_key[$i]];
+      echo '</strong>';
     } else if($result_vote[$o_key[$i]] < $result_vote[$c_key[$i]]){
+      echo '<strong>';
       echo $time[$i].' ... ';
       echo $result_vote[$o_key[$i]].' : ';
       echo '<span class="red">'.$result_vote[$c_key[$i]].'</span>';
+      echo '</strong>';
     } else {
+      echo '<strong>';
       echo $time[$i].' ... ';
       echo $result_vote[$o_key[$i]].' : ';
       echo $result_vote[$c_key[$i]];
+      echo '</strong>';
     }
-    echo '</strong>';
+
   }else {
     echo '<span class="gray">';
-    if($result_vote[$o_key[$i]] > $result_vote[$c_key[$i]]){
-      echo $time[$i].' ... ';
-      echo $result_vote[$o_key[$i]].' : ';
-      echo $result_vote[$c_key[$i]];
-    } else if($result_vote[$o_key[$i]] < $result_vote[$c_key[$i]]){
-      echo $time[$i].' ... ';
-      echo $result_vote[$o_key[$i]].' : ';
-      echo $result_vote[$c_key[$i]];
-    } else {
-      echo $time[$i].' ... ';
-      echo $result_vote[$o_key[$i]].' : ';
-      echo $result_vote[$c_key[$i]];
-    }
-  echo '</span>';
-}
+    echo $time[$i].' ... ';
+    echo $result_vote[$o_key[$i]].' : ';
+    echo $result_vote[$c_key[$i]];
+    echo '</span>';
+  }
   echo '<br>';
 }
 
