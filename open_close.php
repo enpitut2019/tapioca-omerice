e472a44d4916b120ab7f15d643d99528505372e1
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-08-05 10:56:02 +0900

    データベースに票数を送る

--- open_close.php
+++ open_close.php
@@ -7,17 +7,16 @@
 $url = parse_url(getenv('DATABASE_URL'));
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($_POST);
+// var_dump($_POST);
+
 // 投票数を取得
 $stmt_vote = $pdo->query('SELECT * FROM sample0801_db LEFT JOIN sample0802_open ON sample0801_db.store_id = sample0802_open.store_id left join sample0802_close on sample0801_db.store_id = sample0802_close.store_id WHERE sample0801_db.store_id ='.$_POST['store_id']);
 $result_vote = $stmt_vote -> fetch(PDO::FETCH_ASSOC);
 
 if(strcmp($_POST['vote_open'], '営業中') == 0) { // 営業中
-  echo '営業中';
   $result_vote['o0_2']+=1;
   $stmt = $pdo->prepare('UPDATE sample0802_open SET o0_2='.$result_vote['o0_2'].'WHERE store_id='.$_POST['store_id']);
   $stmt->execute();
-
 } else if(strcmp($_POST['vote_close'], '閉店中') == 0) { // 閉店中
   $result_vote['c0_2']+=1;
   $stmt = $pdo->prepare('UPDATE sample0802_close SET c0_2='.$result_vote['c0_2'].'WHERE store_id='.$_POST['store_id']);
@@ -26,8 +25,8 @@ if(strcmp($_POST['vote_open'], '営業中') == 0) { // 営業中
 
 
 
-// header('Location:https://tapiome.herokuapp.com/store_info_count.php?store_id='.$_POST['store_id']);
-// exit();
+header('Location:https://tapiome.herokuapp.com/store_info_count.php?store_id='.$_POST['store_id']);
+exit();
 
 ?>
 </html>
