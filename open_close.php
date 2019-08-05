4309343913b3bb80503c081863af42d7d9dfc8ac
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-08-05 10:32:25 +0900

    データベースに票数を送る

--- open_close.php
+++ open_close.php
@@ -10,6 +10,7 @@ $result_vote = $stmt_vote -> fetch(PDO::FETCH_ASSOC);
 
 
 if(strcmp($_POST['vote'], '営業中') == 0) { // 営業中
+  echo '営業中'
   $result_vote['o0_2']+=1;
   $stmt = $pdo->prepare('UPDATE sample0802_open SET o0_2='.$result_vote['o0_2'].'WHERE store_id='.$_POST['store_id']);
   $stmt->execute();
@@ -19,6 +20,8 @@ if(strcmp($_POST['vote'], '営業中') == 0) { // 営業中
   $stmt = $pdo->prepare('UPDATE sample0802_close SET c0_2='.$result_vote['c0_2'].'WHERE store_id='.$_POST['store_id']);
   $stmt->execute();
 }
+
+
 
 // header('Location:https://tapiome.herokuapp.com/store_info_count.php?store_id='.$_POST['store_id']);
 // exit();
