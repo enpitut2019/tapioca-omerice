d82bcd1fab3e26a40645c0608a809884c399763d
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-08-05 13:53:52 +0900

    時間ごと,a

--- open_close.php
+++ open_close.php
@@ -45,8 +45,8 @@ if(strcmp($_POST['vote_open'], '営業中') == 0) { // 営業中
 
 
 
- // header('Location:https://tapiome.herokuapp.com/store_info_count.php?store_id='.$_POST['store_id']);
- // exit();
+ header('Location:https://tapiome.herokuapp.com/store_info_count.php?store_id='.$_POST['store_id']);
+ exit();
 
 ?>
 </html>
