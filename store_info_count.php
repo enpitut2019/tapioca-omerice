6bf7be8c2909248e7fa166b3770e8cbe3d9765d4
Shiknai <rickey.bb0918@gmail.com>
2019-08-05 14:20:54 +0900

    太め

--- store_info_count.php
+++ store_info_count.php
@@ -43,15 +43,15 @@ $stmt_vote = $pdo->query('SELECT * FROM sample0801_db LEFT JOIN sample0802_open 
 $result_vote = $stmt_vote -> fetch(PDO::FETCH_ASSOC);
 
 // 投票のハッシュのキーの配列
-$o_key = array('o4_6','o6_8','o8_10','o10_12','o12_14','o14_16','o16_18','o18_20','o20_22','o22_24','o0_2','o2_4');
-$c_key = array('c4_6','c6_8','c8_10','c10_12','c12_14','c14_16','c16_18','c18_20','c20_22','c22_24','c0_2','c2_4');
-$time = array(' 4:00~ 6:00', ' 6:00~ 8:00', ' 8:00~10:00', '10:00~12:00', '12:00~14:00', '14:00~16:00', '16:00~18:00', '18:00~20:00', '20:00~22:00', '22:00~ 0:00', ' 0:00~ 2:00', ' 2:00~ 4:00');
+$o_key = array('o0_2','o2_4', 'o4_6','o6_8','o8_10','o10_12','o12_14','o14_16','o16_18','o18_20','o20_22','o22_24');
+$c_key = array('c0_2','c2_4', 'c4_6','c6_8','c8_10','c10_12','c12_14','c14_16','c16_18','c18_20','c20_22','c22_24');
+$time = array( ' 0:00~ 2:00', ' 2:00~ 4:00', ' 4:00~ 6:00', ' 6:00~ 8:00', ' 8:00~10:00', '10:00~12:00', '12:00~14:00', '14:00~16:00', '16:00~18:00', '18:00~20:00', '20:00~22:00', '22:00~ 0:00');
 date_default_timezone_set('Asia/Tokyo');
 $date = date("H");
 
 // 投票数の表示
 for ($i = 0; $i < 12; $i++) {
-  for($j = 4; $j < 28; $j = $j+2) {
+  for($j = 0; $j < 24; $j = $j+2) {
     if($j <= intval($date) && intval($date) < $j+2) {
       if($result_vote[$o_key[$i]] > $result_vote[$c_key[$i]]){
         echo '<strong>'.$time[$i].' ... </strong>';
@@ -67,6 +67,7 @@ for ($i = 0; $i < 12; $i++) {
         echo '<strong>'.$result_vote[$c_key[$i]]'</strong>';
       }
     }else {
+      if($result_vote[$o_key[$i]] > $result_vote[$c_key[$i]]){
       echo $time[$i].' ... ';
       echo $result_vote[$o_key[$i]].' : ';
       echo $result_vote[$c_key[$i]];
