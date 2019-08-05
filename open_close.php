e21750924cb094ab752751f935dad7c21c39a078
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-08-05 10:46:27 +0900

    データベースに票数を送る

--- open_close.php
+++ open_close.php
@@ -7,11 +7,10 @@
 $url = parse_url(getenv('DATABASE_URL'));
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($_POST);
 // 投票数を取得
-$stmt_vote = $pdo->query('SELECT * FROM sample0801_db LEFT JOIN sample0802_open ON sample0801_db.store_id = sample0802_open.store_id left join sample0802_close on sample0801_db.store_id = sample0802_close.store_id WHERE sample0801_db.store_id ='.$store_id);
+$stmt_vote = $pdo->query('SELECT * FROM sample0801_db LEFT JOIN sample0802_open ON sample0801_db.store_id = sample0802_open.store_id left join sample0802_close on sample0801_db.store_id = sample0802_close.store_id WHERE sample0801_db.store_id ='.$_POST['store_id']);
 $result_vote = $stmt_vote -> fetch(PDO::FETCH_ASSOC);
-
+var_dump($_POST);
 if(strcmp($_POST['vote_open'], '営業中') == 0) { // 営業中
   echo '営業中';
   $result_vote['o0_2']+=1;
