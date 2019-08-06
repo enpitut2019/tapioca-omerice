264b41feed7662f67712569f8f81cfd658673dd6
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 22:39:05 +0900

    open_flag

--- index.php
+++ index.php
@@ -40,22 +40,27 @@ if($wd == 1){
   $monday = '日曜日';
 }
 
+
 $stmt99 = $pdo->prepare('SELECT * FROM sample0801_db LEFT JOIN info ON sample0801_db.store_id = info.store_id');
 $stmt98 = $pdo->prepare('UPDATE info SET status = 1');
 $stmt97 = $pdo->prepare('UPDATE info SET status = 0');
-
-$result99 = $stmt99 -> fetch(PDO::FETCH_ASSOC);
-var_dump($result99);
+$stmt99->execute();
+$stmt98->execute();
+$stmt97->execute();
 
 if($stmt99) {
   while($result99 = $stmt99 -> fetch(PDO::FETCH_ASSOC)) {
+    var_dump($result99);
     if(($result99['l_time_o'] < $open_flag && $open_flag < $result99['l_time_c']) || ($result99['d_time_o'] < $open_flag && $open_flag < $result99['d_time_c'])) {
-      $stmt98 -> execute();
+      //$stmt98 -> execute();
+      $result98 = $stmt98 -> fetch(PDO::FETCH_ASSOC);
       if($result99['holiday'] == $monday){
-        $stmt97 -> execute();
+        $result97 = $stmt97 -> fetch(PDO::FETCH_ASSOC);
+        //$stmt97 -> execute();
       }
     }else {
-      $stmt97 -> execute();
+      //$stmt97 -> execute();
+      $result97 = $stmt97 -> fetch(PDO::FETCH_ASSOC);
     }
   }
 }
