ebdf124cc77672f7d106ff97bb8442619c83069f
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 15:42:15 +0900

    検索の合成

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ for($i=0; $i<8; $i++) {
       $stmt11 = $pdo->prepare('SELECT * FROM sample0801_db LEFT JOIN info ON sample0801_db.store_id = info.store_id WHERE store_name like :word AND status=1');
       $stmt11->bindValue(':word', '%'.$word.'%', PDO::PARAM_STR);
       $stmt11->execute();
-      if($stmt){
+      if($stmt11){
         while($result11 = $stmt11 -> fetch(PDO::FETCH_ASSOC)) {
          echo $result11['store_name'];
          echo '：<a href ="https://tapiome.herokuapp.com/store_info_count.php?store_id='.$result11['store_id'].'">詳細情報</a><br>';
