32e5410aad1ac66f0d350c1709e651e2e1e5c19e
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 23:05:50 +0900

    open_flag

--- index.php
+++ index.php
@@ -49,14 +49,14 @@ $stmt99->execute();
 if($stmt99) {
   while($result99 = $stmt99 -> fetch(PDO::FETCH_ASSOC)) {
     if(($result99['l_time_o'] < $open_flag && $open_flag < $result99['l_time_c']) || ($result99['d_time_o'] < $open_flag && $open_flag < $result99['d_time_c'])) {
-      $stmt98 -> execute();
+      $result99 = $stmt98 -> execute();
       echo $result99['status'];
       if($result99['holiday'] == $monday){
         $stmt97 -> execute();
       }
     }else {
-      $stmt97 -> execute();
-      //echo $result99['status'];
+      $result99 = $stmt97 -> execute();
+      echo $result99['status'];
     }
   }
 }
