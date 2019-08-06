b7db2cf5c1a15546ee3db170041d2d61bfae9be0
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 14:07:28 +0900

    検索の合成

--- index.php
+++ index.php
@@ -50,13 +50,9 @@ for($i=0; $i<8; $i++) {
 
 <!-- 検索フォーム -->
 <form method="post" action="index.php">
-<input type="search" name="kensaku" ><input type="submit" value="検索">
-</form>
-
-<!-- 今から営業中のチェックボックボックスを作りたい -->
-<form method="post" action="index.php">
+<input type="search" name="kensaku" >
 <input type="checkbox" name="eigyou[]" value="営業中">営業中店舗のみ表示
-<input type="submit" value="表示">
+<input type="submit" value="検索">
 </form>
 
 <?php
