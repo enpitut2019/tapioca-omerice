61350ab944e8c9dcf41b8dbad5dc2245fbaadc4b
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-08-06 11:00:16 +0900

    SQLインジェクション:open_close

--- open_close.php
+++ open_close.php
@@ -10,8 +10,9 @@ $pdo = new PDO($dsn, $url['user'], $url['pass']);
 // var_dump($_POST);
 
 // 投票数を取得
+$dtore_id = $_POST['store_id'];
 $stmt_vote = $pdo->prepare('SELECT * FROM sample0801_db LEFT JOIN sample0802_open ON sample0801_db.store_id = sample0802_open.store_id left join sample0802_close on sample0801_db.store_id = sample0802_close.store_id WHERE sample0801_db.store_id = ?');
-$stmt_vote->bindValue(1, $_POST['store_id'], PDO::PARAM_INT);
+$stmt_vote->bindValue(1, $dtore_id, PDO::PARAM_INT);
 var_dump($stmt_vote);
 $execute();
 $result_vote = $stmt_vote -> fetch(PDO::FETCH_ASSOC);
