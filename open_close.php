effc1a5947e96a061480d9653034c4070d191758
sakiANZAI <tayloref3127@gmail.com>
2019-08-06 23:44:41 +0900

    連続投票を防ぐ

--- open_close.php
+++ open_close.php
@@ -39,7 +39,10 @@ $date = date("H");
 $index = intval($date/2);
 
 //セッションの有効期限を120分に設定
-
+// session_set_cookie_params(30);
+ini_set('session.gc_maxlifetime', 20);
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 1);
 // セッション管理開始
 session_start();
 
@@ -52,7 +55,6 @@ var_dump($_SESSION[$session_key]);
 if (!isset($_SESSION[$session_key])) {
     // キー'$store_id'が登録されていなければ、1を設定
     echo "ない";
-    session_set_cookie_params(30);
     $_SESSION[$session_key] = 1;
 } else {
     //  キー'$store_id'が登録されていれば、その値をインクリメント
