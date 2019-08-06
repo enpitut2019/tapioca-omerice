ee30699c8610f43c1250727e85a9c3f93a16bde0
Shiknai <rickey.bb0918@gmail.com>
2019-08-07 01:06:12 +0900

    大事件

--- index.php
+++ index.php
@@ -48,13 +48,13 @@ $stmt99 -> execute();
 if($stmt99) {
   while($result99 = $stmt99 -> fetch(PDO::FETCH_ASSOC)) {
     if(($result99['l_time_o'] < $open_flag && $open_flag < $result99['l_time_c']) || ($result99['d_time_o'] < $open_flag && $open_flag < $result99['d_time_c'])) {
-      $stmt97 -> execute();
+      $stmt98 -> execute();
       var_dump($result99);
       if($result99['holiday'] == $monday){
         $stmt97 -> execute();
       }
     }else {
-      $result99 = $stmt97 -> execute();
+      $stmt97 -> execute();
       var_dump($result99);
     }
   }
