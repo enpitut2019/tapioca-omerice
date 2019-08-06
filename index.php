fdcd38ed145878fd61850113c6cf067dd6517403
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 22:05:08 +0900

    open_flag

--- index.php
+++ index.php
@@ -107,11 +107,11 @@ for($i=0; $i<8; $i++) {
       $stmt11->execute();
       if($stmt11){
         while($result11 = $stmt11 -> fetch(PDO::FETCH_ASSOC)) {
-          if(($result11['l_time_o'] < $open_flag && $open_falg < $result11['l_time_c']) || ($result11['d_time_o'] < $open_flag && $open_flag < $result11['d_time_c'])) {
+          if(($result11['l_time_o'] < $open_flag && $open_flag < $result11['l_time_c']) || ($result11['d_time_o'] < $open_flag && $open_flag < $result11['d_time_c'])) {
             $rikiya = '営業時間内';
             $stmt98 -> execute();
-            var_dump($result['l_time_o']);
-            var_dump($open_frag);
+            var_dump($result11['l_time_o']);
+            var_dump($open_flag);
             if($result11['holiday'] == $monday){
               $rikiya = '営業時間外';
               $stmt97 -> execute();
