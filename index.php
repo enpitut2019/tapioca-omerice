cafb12e6a8b39e087ccac9097ea26962ff18d491
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 15:58:25 +0900

    検索の合成

--- index.php
+++ index.php
@@ -69,6 +69,7 @@ for($i=0; $i<8; $i++) {
          echo '：<a href ="https://tapiome.herokuapp.com/store_info_count.php?store_id='.$result11['store_id'].'">詳細情報</a><br>';
          echo '良い11';
        }
+     } 
      } else {
        echo 'dame10';
      }
