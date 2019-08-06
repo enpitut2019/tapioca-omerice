99d87fe9245f82ec882d6ab57b62f69ec26d00c7
sakiANZAI <tayloref3127@gmail.com>
2019-08-06 23:02:51 +0900

    連続投票を防ぐ

--- open_close.php
+++ open_close.php
@@ -39,21 +39,34 @@ $date = date("H");
 $index = intval($date/2);
 
 // セッションの有効期限を120分に設定
-session_set_cookie_params(60 * 120);
-// セッション管理開始
-session_start();
-var_dump($_SESSION[$store_id]);
-if (!isset($_SESSION[$store_id])) {
+// session_set_cookie_params(60 * 120);
+// // セッション管理開始
+// session_start();
+// var_dump($_SESSION[$store_id]);
+// if (!isset($_SESSION[$store_id])) {
+//     // キー'$store_id'が登録されていなければ、1を設定
+//     echo "ない";
+//     $_SESSION[$store_id] = 1;
+// } else {
+//     //  キー'$store_id'が登録されていれば、その値をインクリメント
+//     echo "ある";
+//     $_SESSION[$store_id]++;
+// }
+// var_dump($_SESSION[$store_id]);
+
+var_dump($_COOKIE[$store_id]);
+if (!isset($_COOKIE[$store_id])) {
     // キー'$store_id'が登録されていなければ、1を設定
     echo "ない";
-    $_SESSION[$store_id] = 1;
+    setcookie($store_id, 1, 300);
 } else {
     //  キー'$store_id'が登録されていれば、その値をインクリメント
     echo "ある";
-    $_SESSION[$store_id]++;
+    setcookie($store_id, 2, 300);
 }
-var_dump($_SESSION[$store_id]);
-if($_SESSION[$store_id] != 1) {
+var_dump($_COOKIE[$store_id]);
+
+if($_COOKIE[$store_id] != 1) {
   if(strcmp($_POST['vote_open'], '営業中') == 0) { // 営業中
     $result_vote[ $o_key[$index]]+=1;
     // $stmt = $pdo->prepare('UPDATE sample0802_open SET '.$o_key[$index].'='.$result_vote[$o_key[$index]].'WHERE store_id='.$_POST['store_id']);
@@ -100,10 +113,10 @@ if($_SESSION[$store_id] != 1) {
 }
 ?>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
   setTimeout(function(){
  window.location.href = 'https://tapiome.herokuapp.com/store_info_count.php?store_id=<?php echo $_POST['store_id']; ?>';
 }, 3*1000);
-</script>
+</script> -->
 </body>
 </html>
