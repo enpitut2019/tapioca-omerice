e2eb7644ed14e8b11ca339852164da182cf5bd5c
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 10:42:42 +0900

    var_dump

--- result_open.php
+++ result_open.php
@@ -23,18 +23,21 @@ $pdo = new PDO($dsn, $url['user'], $url['pass']);
     while($result = $stmt2 -> fetch(PDO::FETCH_ASSOC)) {
       $stmt = $pdo->query('SELECT * FROM sample0801_db WHERE store_id = '.$result['store_id']);
       $result2 = $stmt -> fetch(PDO::FETCH_ASSOC);
-      var_dump($result);
-      echo '</br>';
-      var_dump($result2);
-      echo '</br>';
+      // var_dump($result);
+      // echo '</br>';
+      // var_dump($result2);
+      // echo '</br>';
+      echo $result['store_name'];
+      echo '：<a href ="https://tapiome.herokuapp.com/store_info_count.php?store_id='.$result['store_id'].'">詳細情報</a><br>';
     }
 
-    if($stmt){
-      while($result = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-        echo $result['store_name'];
-        echo '：<a href ="https://tapiome.herokuapp.com/store_info_count.php?store_id='.$result['store_id'].'">詳細情報</a><br>';
-      }
-    }else{
+    // if($stmt){
+    //   while($result = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+    //     echo $result['store_name'];
+    //     echo '：<a href ="https://tapiome.herokuapp.com/store_info_count.php?store_id='.$result['store_id'].'">詳細情報</a><br>';
+    //   }
+    // }
+    else{
       echo '営業中の店舗はありません';
     }
   }
