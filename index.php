94fdd483fd376e270d60c8ebb4d1beace13481f9
Shiknai <rickey.bb0918@gmail.com>
2019-08-06 23:43:39 +0900

    open_flag

--- index.php
+++ index.php
@@ -45,14 +45,12 @@ $stmt98 = $pdo->prepare('UPDATE info SET status = 1');
 $stmt97 = $pdo->prepare('UPDATE info SET status = 0');
 $stmt99->execute();
 
-if($stmt98 -> execute()) {
-  echo 'true';
-}
 if($stmt99) {
   while($result99 = $stmt99 -> fetch(PDO::FETCH_ASSOC)) {
     if(($result99['l_time_o'] < $open_flag && $open_flag < $result99['l_time_c']) || ($result99['d_time_o'] < $open_flag && $open_flag < $result99['d_time_c'])) {
       $stmt98 -> execute();
-      echo $result99['status'];
+      echo 'b';
+      //echo $result99['status'];
       if($result99['holiday'] == $monday){
         $stmt97 -> execute();
       }
