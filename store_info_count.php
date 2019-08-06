e6270e24a7d0e9c90af251be9e00cdbfec12f501
tomoya-onuki <tomoya.onuki28@gmail.com>
2019-08-07 02:41:09 +0900

    twitter埋め込みデザイン

--- store_info_count.php
+++ store_info_count.php
@@ -139,7 +139,7 @@ echo '</p>';
 <?php
 if($result_detail_info['twitter']){
 ?>
-<a class="twitter-timeline" data-width="400" data-height="400" data-link-color="#2B7BB9" href="https://twitter.com/<?php echo h($result_detail_info['twitter']); ?>?ref_src=twsrc%5Etfw">Tweets by <?php echo h($result_detail_info['twitter']); ?></a>
+<a class="twitter-timeline" width="400" height="400" data-link-color="#2B7BB9" href="https://twitter.com/<?php echo h($result_detail_info['twitter']); ?>?ref_src=twsrc%5Etfw">Tweets by <?php echo h($result_detail_info['twitter']); ?></a>
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 <?php
 }
